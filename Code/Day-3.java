f5716176ce437273dc500a0303dc29e09af71ec0
Yumna Mubeen <141240511+Yumna0019@users.noreply.github.com>
2024-09-11 18:23:49 +0500

    Add files via upload

---
+++ Code/Day-3.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args){
+         Scanner scanner = new Scanner(System.in); 
+        
+        int n = scanner.nextInt(); 
+        scanner.close(); 
+        
+        if (n%2 != 0){
+            System.out.println("Weird");
+        }
+        else {
+            if (n>=2 && n<=5){
+                System.out.println("Not Weird");
+            }
+            if (n>=6 && n<=20){
+                System.out.println("Weird");
+            }
+            if (n>20){
+                System.out.println("Not Weird");
+            }
+            
+        }
+    }
+}

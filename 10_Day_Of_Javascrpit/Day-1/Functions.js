77956bee9ce7ea9bbf5c051880c74190ef39324b
Yumna <yumnamubeen0019@gmail.com>
2024-10-11 10:48:01 +0500

    10_Day_Of_Javascrpit

---
+++ 10_Day_Of_Javascrpit/Day-1/Functions.js
@@ -0,0 +1,20 @@
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+function factorial(n){
+    let fact = 1;
+    for (let i = 1; i <= n; i++){
+        fact *= i;
+    }
+    return fact;
+    
+}
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}

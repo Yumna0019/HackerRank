f6be779c1375e4bbf8860d6a02a51b7ff9431dee
Yumna <yumnamubeen0019@gmail.com>
2024-10-25 11:30:54 +0500

    10_Day_Of_Javascrpit

---
+++ 10_Day_Of_Javascrpit/Day-3/Arrays.js
@@ -0,0 +1,24 @@
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    let uniqueNums = Array.from(new Set(nums));
+    
+    uniqueNums.sort((a,b) => b - a );
+ 
+    return uniqueNums[1];
+}
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
